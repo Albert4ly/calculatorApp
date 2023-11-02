89517af7edd863b426f4aa2568b321662aa4ed3b
Albert <a91614830@gmail.com>
2023-11-02 12:49:25 +0100

    feat:added-charactersAllocation-function-to-created-variables

--- js/calculator.js
+++ js/calculator.js
@@ -3,9 +3,38 @@ const displayPushedNr = document.querySelector('.last__pushed__nr');
 const displayLastPushedBtns = document.querySelector('.last__pushed__btns');
 
 let currentValueBtn = '';
+let currentPushedNr = '';
+let currentPushedNrClone = '';
+let pushedOperator = '';
+let concatenatedStrs = '';
+
+function charactersAllocation(currentValueBtn) {
+   const signsArr = ['%', 'CE', 'C', 'X', 'f', 'x^', '√', '/', 'x', '*', '-', '+', '+/-', '.', '='];
+   const basicOperatorsArr = ['x', '-', '+', '/', '='];
+
+   if (!signsArr.includes(currentValueBtn)) {
+      if (pushedOperator.length === 0) {
+         currentPushedNr += currentValueBtn;
+         currentPushedNrClone = currentPushedNr;
+      }
+
+      if (pushedOperator.length !== 0) {
+         currentPushedNr = '';
+         currentPushedNr += currentValueBtn;
+         pushedOperator = '';
+      }
+   }
+
+   if (basicOperatorsArr.includes(currentValueBtn)) {
+      pushedOperator += currentValueBtn;
+      concatenatedStrs = currentPushedNr;
+      concatenatedStrs += currentValueBtn;
+   }
+}
 
 function init(e) {
    currentValueBtn = e.target.value;
+   charactersAllocation(currentValueBtn);
 }
 
 btns.addEventListener('click', init, true);
