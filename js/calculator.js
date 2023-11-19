6ba9586c2c4256d010501f330e45a95724688354
Albert <a91614830@gmail.com>
2023-11-19 17:25:17 +0100

    feat:substraction-funcionality-added

--- js/calculator.js
+++ js/calculator.js
@@ -106,6 +106,10 @@ function result(digitToMathOperation, digitToMathOperation2, pushedOperator) {
       resultOfOperation2 = resultOfOperation;
    }
 
+   if ((pushedOperator === '-') && (digitToMathOperation2 > 1)) {
+      resultOfOperation = digitToMathOperation - digitToMathOperation2;
+      resultOfOperation2 = resultOfOperation;
+   }
    
 }
 
