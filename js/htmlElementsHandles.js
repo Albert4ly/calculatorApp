fe249aad7c428a5639fa8202e51d81f964fcbf4d
Albert <a91614830@gmail.com>
2023-11-21 19:05:24 +0100

    refactor:move-handles-to-separate-file

---
+++ js/htmlElementsHandles.js
@@ -0,0 +1,5 @@
+const btns = document.querySelector('.btns__cnt');
+const displayPushedNr = document.querySelector('.last__pushed__nr');
+const displayLastPushedBtns = document.querySelector('.last__pushed__btns');
+
+export { btns, displayPushedNr, displayLastPushedBtns };
