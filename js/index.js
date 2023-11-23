0c5b79962a8b91b4acf32e18d3e463e1e36f82b7
Albert <a91614830@gmail.com>
2023-11-23 18:22:55 +0100

    refactor:delete-two-handles-with-htmlElementsHandles-import

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { btns, displayPushedNr, displayLastPushedBtns } from "./htmlElementsHandles.js";
-import { init } from "./init.js"
+import { btns } from "./htmlElementsHandles.js";
+import { init } from "./init.js";
 
 btns.addEventListener('click', init);
