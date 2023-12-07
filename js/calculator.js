35361580f91cdb3892cbbae72915f90d53fe400f
Albert <a91614830@gmail.com>
2023-12-07 19:23:05 +0100

    refactor:comment

--- js/calculator.js
+++ js/calculator.js
@@ -1,216 +1,214 @@
-import { displayPushedNr } from "./htmlElementsHandles.js";
-
 class Calculator {
    constructor(currentPushedNr) {
       this.currentPushedNr = currentPushedNr;
    }
 
    displayDigit() {
-      displayPushedNr.innerHTML = this.currentPushedNr;
+      return this.currentPushedNr;
    }
 }
 
 export { Calculator };
 
-let currentValueBtn = '';
-let currentPushedNr = '';
-let currentPushedNrClone = '';
-let currentPushedNrClone2 = '';
-let currentPushedNrClone3 = '';
-let pushedOperator = '';
-let pushedOperatorClone = '';
-let pushedOperatorClone2 = '';
-let concatenatedStrs = '';
+// let currentValueBtn = '';
+// let currentPushedNr = '';
+// let currentPushedNrClone = '';
+// let currentPushedNrClone2 = '';
+// let currentPushedNrClone3 = '';
+// let pushedOperator = '';
+// let pushedOperatorClone = '';
+// let pushedOperatorClone2 = '';
+// let concatenatedStrs = '';
 
-const nrTab = [];
+// const nrTab = [];
 
-let digitToMathOperation = 0;
-let digitToMathOperation2 = 0;
+// let digitToMathOperation = 0;
+// let digitToMathOperation2 = 0;
 
-let resultOfOperation = 0;
-let resultOfOperation2 = 0;
-let resultOfOperation3 = 0;
+// let resultOfOperation = 0;
+// let resultOfOperation2 = 0;
+// let resultOfOperation3 = 0;
 
-const basicOperatorsArr = ['x', '-', '+', '/', '='];
+// const basicOperatorsArr = ['x', '-', '+', '/', '='];
 
-function displayLastDigit(currentPushedNr, currentPushedNrClone, currentPushedNrClone2, currentPushedNrClone3) {
-   if (currentValueBtn.includes('X')) {
-      currentPushedNr = currentPushedNr.substring(0, currentPushedNr.length - 1);
-      currentPushedNrClone = currentPushedNr;
-      currentPushedNrClone2 = currentPushedNr;
-      currentPushedNrClone3 = currentPushedNr;
-      displayPushedNr.innerHTML = currentPushedNr;
-   }
+// function displayLastDigit(currentPushedNr, currentPushedNrClone, currentPushedNrClone2, currentPushedNrClone3) {
+//    if (currentValueBtn.includes('X')) {
+//       currentPushedNr = currentPushedNr.substring(0, currentPushedNr.length - 1);
+//       currentPushedNrClone = currentPushedNr;
+//       currentPushedNrClone2 = currentPushedNr;
+//       currentPushedNrClone3 = currentPushedNr;
+//       displayPushedNr.innerHTML = currentPushedNr;
+//    }
  
-}
+// }
 
-function compounding(currentPushedNr, currentPushedNrClone, currentPushedNrClone2, currentPushedNrClone3) {
+// function compounding(currentPushedNr, currentPushedNrClone, currentPushedNrClone2, currentPushedNrClone3) {
    
-   if (currentValueBtn.includes('x^')) {
-      let digit = Number(currentPushedNr);
-      currentPushedNr = Math.pow(digit, 2);
-      currentPushedNrClone = currentPushedNr;
-      currentPushedNrClone2 = currentPushedNr;
-      currentPushedNrClone3 = currentPushedNr;
-      displayPushedNr.innerHTML = currentPushedNr;
-   }
+//    if (currentValueBtn.includes('x^')) {
+//       let digit = Number(currentPushedNr);
+//       currentPushedNr = Math.pow(digit, 2);
+//       currentPushedNrClone = currentPushedNr;
+//       currentPushedNrClone2 = currentPushedNr;
+//       currentPushedNrClone3 = currentPushedNr;
+//       displayPushedNr.innerHTML = currentPushedNr;
+//    }
    
-}
+// }
 
 
-function clearDisplay(currentValueBtn) {
+// function clearDisplay(currentValueBtn) {
 
-   if (currentValueBtn.includes('CE')) {
-   currentValueBtn = '';
-   currentPushedNr = '';
-   currentPushedNrClone = '';
-   currentPushedNrClone2 = '';
-   currentPushedNrClone3 = '';
-   pushedOperator = '';
-   pushedOperatorClone = '';
-   pushedOperatorClone2 = '';
-   concatenatedStrs = '';
+//    if (currentValueBtn.includes('CE')) {
+//    currentValueBtn = '';
+//    currentPushedNr = '';
+//    currentPushedNrClone = '';
+//    currentPushedNrClone2 = '';
+//    currentPushedNrClone3 = '';
+//    pushedOperator = '';
+//    pushedOperatorClone = '';
+//    pushedOperatorClone2 = '';
+//    concatenatedStrs = '';
 
-   nrTab.length = 0;
+//    nrTab.length = 0;
 
-   digitToMathOperation = 0;
-   digitToMathOperation2 = 0;
+//    digitToMathOperation = 0;
+//    digitToMathOperation2 = 0;
 
-   resultOfOperation = 0;
-   resultOfOperation2 = 0;
-   resultOfOperation3 = 0;
-   }
+//    resultOfOperation = 0;
+//    resultOfOperation2 = 0;
+//    resultOfOperation3 = 0;
+//    }
 
-}
+// }
 
-function resetAfterResult() {
-   currentPushedNrClone = '';
-   currentPushedNrClone2 = '';
-}
+// function resetAfterResult() {
+//    currentPushedNrClone = '';
+//    currentPushedNrClone2 = '';
+// }
 
-function displayNr(currentPushedNr, resultOfOperation, pushedOperator, newStr) {
+// function displayNr(currentPushedNr, resultOfOperation, pushedOperator, newStr) {
    
-   if ((resultOfOperation < 1) || (pushedOperator.length < 1)) {
-      displayPushedNr.innerHTML = currentPushedNr;
-   } 
+//    if ((resultOfOperation < 1) || (pushedOperator.length < 1)) {
+//       displayPushedNr.innerHTML = currentPushedNr;
+//    } 
 
 
-   if (resultOfOperation > 1 && pushedOperator.length > 0) {
-      displayPushedNr.innerHTML = resultOfOperation;
-      resetAfterResult();
-   }
+//    if (resultOfOperation > 1 && pushedOperator.length > 0) {
+//       displayPushedNr.innerHTML = resultOfOperation;
+//       resetAfterResult();
+//    }
 
-}
+// }
 
-function displayLastBtns(concatenatedStrs) {
+// function displayLastBtns(concatenatedStrs) {
 
-   if ((resultOfOperation < 1) || (pushedOperator.length < 1)) {
-      displayLastPushedBtns.innerHTML = concatenatedStrs;
-   }
+//    if ((resultOfOperation < 1) || (pushedOperator.length < 1)) {
+//       displayLastPushedBtns.innerHTML = concatenatedStrs;
+//    }
 
-   if ((resultOfOperation > 1) && (pushedOperator.length < 1)) {
-      displayLastPushedBtns.innerHTML = resultOfOperation2 + pushedOperatorClone2;
-   }
+//    if ((resultOfOperation > 1) && (pushedOperator.length < 1)) {
+//       displayLastPushedBtns.innerHTML = resultOfOperation2 + pushedOperatorClone2;
+//    }
    
-   if ((resultOfOperation > 1) && (pushedOperator.length > 0)) {
-      displayLastPushedBtns.innerHTML = resultOfOperation + pushedOperator;
-   }
+//    if ((resultOfOperation > 1) && (pushedOperator.length > 0)) {
+//       displayLastPushedBtns.innerHTML = resultOfOperation + pushedOperator;
+//    }
    
-}
+// }
 
-function result(digitToMathOperation, digitToMathOperation2, pushedOperator) {
+// function result(digitToMathOperation, digitToMathOperation2, pushedOperator) {
 
-   if ((pushedOperator === '+') && (digitToMathOperation2 > 1) ) {
-      resultOfOperation = digitToMathOperation + digitToMathOperation2;
-      resultOfOperation2 = resultOfOperation;
-   }
+//    if ((pushedOperator === '+') && (digitToMathOperation2 > 1) ) {
+//       resultOfOperation = digitToMathOperation + digitToMathOperation2;
+//       resultOfOperation2 = resultOfOperation;
+//    }
 
-   if ((pushedOperator === 'x') && (digitToMathOperation2 > 1)) {
-      resultOfOperation = digitToMathOperation * digitToMathOperation2;
-      resultOfOperation2 = resultOfOperation;
-   }
+//    if ((pushedOperator === 'x') && (digitToMathOperation2 > 1)) {
+//       resultOfOperation = digitToMathOperation * digitToMathOperation2;
+//       resultOfOperation2 = resultOfOperation;
+//    }
 
-   if ((pushedOperator === '-') && (digitToMathOperation2 > 1)) {
-      resultOfOperation = digitToMathOperation - digitToMathOperation2;
-      resultOfOperation2 = resultOfOperation;
-   }
+//    if ((pushedOperator === '-') && (digitToMathOperation2 > 1)) {
+//       resultOfOperation = digitToMathOperation - digitToMathOperation2;
+//       resultOfOperation2 = resultOfOperation;
+//    }
    
-   if ((pushedOperator === '/') && (digitToMathOperation2 > 1)) {
-      resultOfOperation = digitToMathOperation / digitToMathOperation2;
-      resultOfOperation2 = resultOfOperation;
-   }
+//    if ((pushedOperator === '/') && (digitToMathOperation2 > 1)) {
+//       resultOfOperation = digitToMathOperation / digitToMathOperation2;
+//       resultOfOperation2 = resultOfOperation;
+//    }
 
-}
+// }
 
-function dependingOf(pushedOperator) {
+// function dependingOf(pushedOperator) {
 
-   if (basicOperatorsArr.includes(pushedOperator)) {
-      concatenateStrs(currentPushedNrClone3, pushedOperator)
-   }
+//    if (basicOperatorsArr.includes(pushedOperator)) {
+//       concatenateStrs(currentPushedNrClone3, pushedOperator)
+//    }
 
-}
+// }
 
-function concatenateStrs(currentPushedNrClone3, pushedOperator) {
-   concatenatedStrs = currentPushedNrClone3 + pushedOperator;
-}
+// function concatenateStrs(currentPushedNrClone3, pushedOperator) {
+//    concatenatedStrs = currentPushedNrClone3 + pushedOperator;
+// }
 
-function dataTypeConversion(resultOfOperation2, nrTab) {
+// function dataTypeConversion(resultOfOperation2, nrTab) {
     
-   if(nrTab.length > 1){
-      digitToMathOperation = Number(nrTab[0]);
-      digitToMathOperation2 = Number(nrTab[1]);
-      nrTab.length = 0;
-   }
+//    if(nrTab.length > 1){
+//       digitToMathOperation = Number(nrTab[0]);
+//       digitToMathOperation2 = Number(nrTab[1]);
+//       nrTab.length = 0;
+//    }
 
-   if((nrTab.length < 2 ) && (resultOfOperation2 !== 0)){
-      digitToMathOperation = Number(resultOfOperation2);
-      digitToMathOperation2 = Number(nrTab[0])
-      nrTab.length = 0;
-   }
+//    if((nrTab.length < 2 ) && (resultOfOperation2 !== 0)){
+//       digitToMathOperation = Number(resultOfOperation2);
+//       digitToMathOperation2 = Number(nrTab[0])
+//       nrTab.length = 0;
+//    }
       
-}
+// }
 
-function charactersAllocation(currentValueBtn) {
-   const signsArr = ['%', 'CE', 'C', 'X', 'f', 'x^', '√', '/', 'x', '*', '-', '+', '+/-', '.', '='];
+// function charactersAllocation(currentValueBtn) {
+//    const signsArr = ['%', 'CE', 'C', 'X', 'f', 'x^', '√', '/', 'x', '*', '-', '+', '+/-', '.', '='];
 
-   if (!signsArr.includes(currentValueBtn)) {
+//    if (!signsArr.includes(currentValueBtn)) {
 
-      if (pushedOperator.length === 0) {
+//       if (pushedOperator.length === 0) {
 
-         // currentPushedNr += currentValueBtn; 
-         currentPushedNrClone = currentPushedNr;
-         currentPushedNrClone3 = currentPushedNrClone;
-      }
+      //    currentPushedNr += currentValueBtn; 
+      //    currentPushedNrClone = currentPushedNr;
+      //    currentPushedNrClone3 = currentPushedNrClone;
+      // }
 
-      if (pushedOperator.length !== 0) {
-         currentPushedNr = '';
-         currentPushedNr = currentValueBtn;
-         currentPushedNrClone2 += currentPushedNr;
-         pushedOperatorClone = pushedOperator;
-         pushedOperatorClone2 = pushedOperator;
-         pushedOperator = '';
-      }
+//       if (pushedOperator.length !== 0) {
+//          currentPushedNr = '';
+//          currentPushedNr = currentValueBtn;
+//          currentPushedNrClone2 += currentPushedNr;
+//          pushedOperatorClone = pushedOperator;
+//          pushedOperatorClone2 = pushedOperator;
+//          pushedOperator = '';
+//       }
 
-   }
+//    }
 
-   if (basicOperatorsArr.includes(currentValueBtn)) {
+//    if (basicOperatorsArr.includes(currentValueBtn)) {
 
-         nrTab.push(currentPushedNr);
-         pushedOperator += currentValueBtn;
-         pushedOperatorClone = pushedOperator;
+//          nrTab.push(currentPushedNr);
+//          pushedOperator += currentValueBtn;
+//          pushedOperatorClone = pushedOperator;
 
-   }
-}
+//    }
+// }
 
-function init(e) {
-   currentValueBtn = e.target.value;
-   clearDisplay(currentValueBtn);
-   charactersAllocation(currentValueBtn);
-   dataTypeConversion(resultOfOperation2, nrTab);
-   result(digitToMathOperation, digitToMathOperation2, pushedOperator);
-   displayNr(currentPushedNr, resultOfOperation, pushedOperator);
-   dependingOf(pushedOperator);
-   displayLastDigit(currentPushedNr, currentPushedNrClone, currentPushedNrClone2, currentPushedNrClone3);
-   compounding(currentPushedNr, currentPushedNrClone, currentPushedNrClone2, currentPushedNrClone3)
-   displayLastBtns(concatenatedStrs);
-}
+// function init(e) {
+   // currentValueBtn = e.target.value;
+   // clearDisplay(currentValueBtn);
+   // charactersAllocation(currentValueBtn);
+   // dataTypeConversion(resultOfOperation2, nrTab);
+   // result(digitToMathOperation, digitToMathOperation2, pushedOperator);
+   // displayNr(currentPushedNr, resultOfOperation, pushedOperator);
+   // dependingOf(pushedOperator);
+   // displayLastDigit(currentPushedNr, currentPushedNrClone, currentPushedNrClone2, currentPushedNrClone3);
+   // compounding(currentPushedNr, currentPushedNrClone, currentPushedNrClone2, currentPushedNrClone3)
+   // displayLastBtns(concatenatedStrs);
+// }
